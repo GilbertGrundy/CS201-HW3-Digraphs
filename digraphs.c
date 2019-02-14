956bd3f3e2154bb674461f0a310134b6cb226253
GilbertGrundy <gilbert.grundy@gmail.com>
2019-02-13 17:40:50 -0800

    Added comments for structure.

--- digraphs.c
+++ digraphs.c
@@ -9,7 +9,10 @@
 int main(int argc, char **argv)
 {
 
-    if(argc != 2)
+    //checks if there is the correct amount of arguments
+    //there should be the filename, flag, and either
+    //a file or directory, if not exit program.
+    if(argc != 3)
     {
         fprintf(stderr, "Digraphs: invalid amount of arguments\n");
         return -1;
@@ -20,11 +23,11 @@ int main(int argc, char **argv)
     if((argv[1][0] == '-') && (argv[1][1] == 't'))
     {
         
-    }
+    }//checks if theflag for a file directory has been passed
     else if((argv[1][0] == '-') && (argv[1][1] == 'p'))
     {
 
-    }
+    }//if not the wrong flag has been passed, exit program.
     else
     {
         fprintf(stderr, "Digraphs: invalid flag\n");
