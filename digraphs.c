9648ad2222373923f385dcb95f182f6b9855b2e5
GilbertGrundy <gilbert.grundy@gmail.com>
2019-02-14 01:58:15 -0800

    Cleaned up comments.

--- digraphs.c
+++ digraphs.c
@@ -14,38 +14,36 @@ int main(int argc, char **argv)
     //a file or directory, if not exit program.
     if(argc != 3)
     {
-        fprintf(stderr, "Digraphs: Invalid amount of arguments\n");
+        fprintf(stderr, "Digraphs: Invalid amount of arguments.\n");
         return -1;
     }
 
     struct digraphs * values;
     struct result top[10];
     result_constructor(top);
-
-//    top[0].count = 0;
-
-  //  printf("Top diagraphs: %s", top[0].digraph);
-    //printf(" %ld\n", top[0].count);
     
-    //to check if main has been passed the flag for
-    //a textfile
+    //checks if main has been passed the flag for a textfile -t
     if((argv[1][0] == '-') && (argv[1][1] == 't'))
     {
          values = digraphs_text(argv[2]);
 
-    }//checks if theflag for a file directory has been passed
+    }//checks if the flag for a file directory has been passed -p
     else if((argv[1][0] == '-') && (argv[1][1] == 'p'))
     {
         values = digraphs_fs(argv[2]);
-    }//if not the wrong flag has been passed, exit program.
+    }//if not, the wrong flag has been passed, exit program.
     else
     {
-        fprintf(stderr, "Digraphs: Invalid flag\n");
+        fprintf(stderr, "Digraphs: Invalid flag.\n");
         return -1;
     }
 
+    //find the top 10 most common digraphs
+    //and place in the result struct
     sort_results(top, values);
 
+    //display the character count, and the top 10
+    //digraphs.
     display(values, top);
 
     return 0;
